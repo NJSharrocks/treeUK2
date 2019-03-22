82ad0f841469312d7bf845553163624ffd44c3f4
njsharrocks <23417048@edgehill.ac.uk>
2019-03-22 09:45:57 +0000

    Database migrations created

---
+++ database/migrations/2019_03_19_145315_create_permissions_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePermissionsRolesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('permissions_roles', function(Blueprint $table)
+		{
+			$table->bigInteger('id', true)->unsigned();
+			$table->string('permissionsID');
+			$table->string('rolesID');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('permissions_roles');
+	}
+
+}
