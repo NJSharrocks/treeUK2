1164d68a8a06c4186f3d26e87947496f4df45d57
njsharrocks <23417048@edgehill.ac.uk>
2019-03-23 14:17:06 +0000

    Added seeder factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Woodland;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        /*//disable foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Model::unguard();
+
+        User::truncate();
+        Woodland::truncate();
+        //re-enable foreign key check for this connection
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');*/
+
+        $this->call(UserTableSeeder::class);
+
+        factory(User::class, 50)->create();
+        factory(Woodland::class, 5)->create();
+
     }
 }
